70b2b94ddc6a14e12373318671dc7bf9b08792b1
daniel <beltrefrancodaniel@gmail.com>
2021-04-19 22:18:30 -0400

    ubicacion de accidentes (mapa)

--- FinalProject/FinalProject/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
+++ FinalProject/FinalProject/obj/Debug/net5.0/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,91 +13,91 @@ namespace FinalProject.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 2 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 2 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 3 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 4 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 5 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 6 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 7 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 8 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 9 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using FinalProject;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
+#line 10 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\_Imports.razor"
 using FinalProject.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
+#line 2 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
+#line 3 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
 using System.Text.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
+#line 4 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
 using System.Text.Json.Serialization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
+#line 5 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
 using Newtonsoft.Json;
 
 #line default
@@ -112,7 +112,7 @@ using Newtonsoft.Json;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 73 "C:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
+#line 73 "c:\Users\daniel\Desktop\clases\6-cuatrimestre\introduccion a ingenieria\IGN-proyecto-final\FinalProject\FinalProject\Pages\Index.razor"
       
 
     string descripcion = "", fecha = "", hora = "", lugar = "", latitud = "", longitud = "";
